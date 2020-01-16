ef5a9b90195e1f2b80c8c5cccc6f1b8d81672ea8
Kaushik Tejani <38485490+KaushikTejani@users.noreply.github.com>
2020-01-16 11:24:24 +0530

    Kirana List Design Done

--- app/src/main/java/codes/tuton/grocery/MainActivity.java
+++ app/src/main/java/codes/tuton/grocery/MainActivity.java
@@ -1,14 +1,50 @@
 package codes.tuton.grocery;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TextView toolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind View
+        bindView();
+
+        //SetToolBar
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_top, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.shoppingCart) {
+            Toast.makeText(this, "Shopping Cart Selected.", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void bindView() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbarTitle = findViewById(R.id.toolbarTitle);
     }
 }
